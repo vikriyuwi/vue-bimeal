6b2f1a5122e26f8207d91a09c51aa7b15ddc17f6
Fikri Yuwi <latifulfikri1902@gmail.com>
2023-06-28 20:48:44 +0700

    user register

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../view/buyer/home/index.vue";
 import Login from "../view/buyer/auth/login.vue";
-import Profile from "../view/buyer/profile/index.vue";
+import Register from "../view/buyer/auth/register.vue";
 import Logout from "../view/buyer/auth/logout.vue";
+import Profile from "../view/buyer/profile/index.vue";
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
   },
   {
     path: "/logout",
